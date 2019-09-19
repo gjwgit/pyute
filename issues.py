8aa02df68950878e9c0e4c6ba6914c4a32e61101
Graham <Graham.Williams@togaware.com>
2019-09-20 09:11:06 +1000

    Allow multiple repos from command line

--- issues.py
+++ issues.py
@@ -10,40 +10,41 @@ import argparse
 option_parser = argparse.ArgumentParser(add_help=False)
 
 option_parser.add_argument(
-    'repo',
+    'repos',
+    nargs="+",
     help='name of github repository as in microsoft/nlp')
 
 args = option_parser.parse_args()
-repo = args.repo
+repos = args.repos
 
 # Cached list of known Microsoft users as generated from orgs.py.
 
 msdevs = [XXXX]
 
-# Obtain all issues and pull requests.
+for repo in repos:
+    
+    # Obtain all issues and pull requests.
 
-issues = {}
-i = 1
+    issues = {}
+    i = 1
 
-while True:
-    response = requests.get('https://api.github.com/repos/' + repo + '/issues?state=all&page=' + str(i) + '&per_page=100').json()
-    if len(response) == 0: break
-    if len(issues) == 0:
-        issues = response
-    else:
-        issues = issues + response
-    i += 1    
+    while True:
+        response = requests.get('https://api.github.com/repos/' + repo + '/issues?state=all&page=' + str(i) + '&per_page=100').json()
+        if len(response) == 0: break
+        if len(issues) == 0:
+            issues = response
+        else:
+            issues = issues + response
+        i += 1    
 
-# print(json.dumps(issues, indent=4))
+    external = internal = 0
 
-external = internal = 0
+    for i in issues:
+        login = i['user']['login']
 
-for i in issues:
-    login = i['user']['login']
+        if login in msdevs:
+            internal += 1
+        else:
+            external += 1
 
-    if login in msdevs:
-        internal += 1
-    else:
-        external += 1
-
-print(repo, internal, external)
+    print(f"{repo},{internal},{external}")
